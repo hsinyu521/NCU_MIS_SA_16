d03d8efc3c6517a316ce75b1351f3624ab84cb53
minyiii <25.7minyi@gmail.com>
2019-12-23 13:29:46 +0800

    老師註冊登入html的json物件都多了一籃option，是為了讓後端去區分他到底是要登入還是註冊，controller測試已成功，能趨辨別是誰傳來的了，耶~~~

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/TeacherController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/TeacherController.java
@@ -33,55 +33,64 @@ public class TeacherController extends HttpServlet {
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {	//1212 6pm by min
-    	//System.out.print("in teacherController doPost\n");
+    	System.out.print("in teacherController doPost\n");
     	
         /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
         
-        /** 取出經解析到JSONObject之Request參數 */
-        String name = jso.getString("name");
-        String email = jso.getString("email");
-        String password = jso.getString("password");
-        String cellphone = jso.getString("cellphone");
-        int gender = jso.getInt("gender");
+        //看要登入還註冊
+        String option = jso.getString("option");
         
-        
-        
-        
-        
-        /** 建立一個新的會員物件 */
-        Teacher t = new Teacher(name, email, password, cellphone, gender);	//建構子1
-        
-        
-        
-        
-        /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
-        if(email.isEmpty() || password.isEmpty() || name.isEmpty() || cellphone.isEmpty() ) {	//
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
+        //若註冊
+        if(option.equals("register")) {
+        	/** 取出經解析到JSONObject之Request參數 */
+        	String name = jso.getString("name");
+        	String email = jso.getString("email");
+        	String password = jso.getString("password");
+        	String cellphone = jso.getString("cellphone");
+        	int gender = jso.getInt("gender");
+        	
+        	System.out.printf("----------teacher註冊---------\n");
+        	//建一個新的老師會員物件 (建構子1)
+        	Teacher t = new Teacher(name, email, password, cellphone, gender);
+        	/** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+            if(email.isEmpty() || password.isEmpty() || name.isEmpty() || cellphone.isEmpty() ) {	//
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
+            /** 透過TeacherHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+            else if (!th.checkDuplicate(t)) {
+                /** 透過TeacherHelper物件的create()方法新建一個會員至資料庫 */
+                JSONObject data = th.create(t);
+                
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "成功! 註冊老師會員資料...");
+                resp.put("response", data);
+                
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
+            else {
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'新增老師會員帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
         }
-        /** 透過TeacherHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
-        else if (!th.checkDuplicate(t)) {
-            /** 透過TeacherHelper物件的create()方法新建一個會員至資料庫 */
-            JSONObject data = th.create(t);
-            
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("message", "成功! 註冊老師會員資料...");
-            resp.put("response", data);
-            
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
-        }
-        else {
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'新增老師會員帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
+        
+        //若為登入
+        else if(option.equals("login")) {	
+        	System.out.printf("----------teacher登入---------\n");
+        	String email = jso.getString("email");
+        	String password = jso.getString("password");
+        	//建一個新的老師會員物件 (建構子4)
+        	Teacher t = new Teacher(email, password);
+        	
         }
     }
 
