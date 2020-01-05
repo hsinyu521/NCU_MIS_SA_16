5946ea61b9b1ab6830c242610c2f946f8e0cd5ec
Chu <lovesky88625@gmail.com>
2020-01-05 19:23:43 +0800

    懶得打

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/ParentController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/ParentController.java
@@ -179,10 +179,18 @@ public class ParentController extends HttpServlet {
         
         /** 新建一個JSONObject用於將回傳之資料進行封裝 */
         JSONObject resp = new JSONObject();
-        resp.put("status", "200");
-        resp.put("message", "家長會員移除成功！");
-        resp.put("response", query);
-
+        System.out.printf("query.getInt(\"row\"):"+query.getInt("row")+"\n");
+        if(query.getInt("row")==1) {
+            resp.put("status", "200");
+            resp.put("message", "家長會員移除成功！");
+            resp.put("response", query);
+        }
+        else {
+        	resp.put("status", "400");
+            resp.put("message", "家長會員移除失敗！該家長還有案件存在！");
+            resp.put("response", query);
+        }
+        
         /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
         jsr.response(resp, response);
 	}
--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/TeacherController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/Group16/controller/TeacherController.java
@@ -151,10 +151,18 @@ public class TeacherController extends HttpServlet {
         
         /** 新建一個JSONObject用於將回傳之資料進行封裝 */
         JSONObject resp = new JSONObject();
-        resp.put("status", "200");
-        resp.put("message", "老師會員移除成功！");
-        resp.put("response", query);
-
+        
+        if(query.getInt("row")==1) {
+        	resp.put("status", "200");
+            resp.put("message", "老師會員移除成功！");
+            resp.put("response", query);
+        }
+        else {
+        	resp.put("status", "400");
+            resp.put("message", "老師會員移除失敗！該老師已有面試資料！");
+            resp.put("response", query);
+        }
+        
         /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
         jsr.response(resp, response);
     }
